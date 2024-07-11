2809b1479542dccbd546e8e83d509a686c349749
Misaka <chuxubank@qq.com>
2024-07-11 16:05:44 +0800

    Rename dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,32 +15,34 @@ repositories {
 }
 
 dependencies {
-    api(libs.junit.junit)
-    api(libs.com.ceridwen.util.ceridwen.util)
-    api(libs.org.apache.commons.commons.lang3)
-    api(libs.commons.net.commons.net)
-    api(libs.commons.logging.commons.logging)
-    api(libs.commons.beanutils.commons.beanutils)
-    api(libs.io.netty.netty.transport)
-    api(libs.io.netty.netty.handler)
-    api(libs.org.bouncycastle.bcpkix.jdk18on)
+    implementation(libs.junit)
+    implementation(libs.com.ceridwen.util)
+    implementation(libs.org.apache.commons.commons.lang3)
+    implementation(libs.commons.net)
+    implementation(libs.commons.logging)
+    implementation(libs.commons.beanutils)
+    implementation(libs.io.netty.transport)
+    implementation(libs.io.netty.handler)
+    implementation(libs.org.bouncycastle.bcpkix.jdk18on)
 }
 
-group = "com.ceridwen.circulation"
+group = "com.xingzhi.circulation"
 version = "2.11.2"
-description = "com.ceridwen.circulation:ceridwen-standard-interchange-protocol-library"
+description = "Ceridwen's SIP Circulation Library for Java"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
     }
 }
 
-tasks.withType<JavaCompile>() {
+tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 
-tasks.withType<Javadoc>() {
+tasks.withType<Javadoc> {
     options.encoding = "UTF-8"
 }

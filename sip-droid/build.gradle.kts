2b3cb96a269ef9470add65a55e658975ed59da25
Misaka <chuxubank@qq.com>
2024-07-12 10:33:12 +0800

    Add sip-droid sub module

---
+++ sip-droid/build.gradle.kts
@@ -0,0 +1,44 @@
+import com.github.jengelman.gradle.plugins.shadow.ShadowExtension
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    `java-library`
+    `maven-publish`
+}
+
+description = "Ceridwen's SIP Circulation Library for Android"
+
+dependencies {
+    implementation(project.rootProject)
+    shadow(libs.champeau.openbeans)
+}
+
+tasks.withType<ShadowJar> {
+    configurations = listOf(project.configurations.compileClasspath.get())
+    dependencies {
+        exclude(dependency("junit:junit"))
+        exclude(dependency("org.hamcrest:hamcrest-core"))
+        exclude(dependency("commons-net:commons-net"))
+        exclude { dep -> dep.moduleGroup == "io.netty" }
+    }
+    relocate("java.beans", "com.googlecode.openbeans")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/OpenXingZhi/sip-library")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("shadow") {
+            project.extensions.getByType<ShadowExtension>().component(this)
+        }
+    }
+}
